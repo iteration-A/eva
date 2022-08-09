c6f59932ad5f9dc7121fc42bb77978c9c7cafeec
Manuel Luna <manuellunada@gmail.com>
2022-08-08 22:18:48 -0500

    fix seconds

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const HOUR = document.querySelector("#Hour tspan");
-const MINUTE = document.querySelector("#Minute tspan");
-const SECOND = document.querySelector("#Second tspan");
+const CLOCK = document.querySelector("#Clock tspan");
+const SECONDS = document.querySelector("#Second tspan")
 
 const RED = "#BD1900";
 const YELLOW = "#E0AD41";
-
-window.addEventListener("pageshow", () => {
-  // fix offset
-  document.querySelector("#Sep2 tspan").setAttribute("y", "680");
-  document.querySelector("#Sep2 tspan").setAttribute("x", "1330");
-  document.querySelector("#Sep2").setAttribute("style", "font-size: 110px");
-  console.log("what");
-});
 
 const updateClock =
   (format = 12) =>
@@ -21,9 +12,11 @@ const updateClock =
       .split(" ");
 
     const [h, m, s] = time.split(":");
-    HOUR.textContent = String(h).padStart(2, "0");
-    MINUTE.textContent = String(m).padStart(2, "0");
-    SECOND.textContent = String(s).padStart(2, "0");
+		const hour = String(h).padStart(2, "0");
+		const minute = String(m).padStart(2, "0");
+		const second = String(s).padStart(2, "0");
+		CLOCK.textContent = `${hour}:${minute}`;
+		SECONDS.textContent = String(second);
   };
 
 setInterval(updateClock(), 1000);
